012e6248ad89ddb4531d82efa6783270c216963e
Amir Mirfallahi <Amir Mirfallahi>
2025-07-16 14:53:51 +0330

    Fixed models and create accounts

--- core/accounts/models/profiles.py
+++ core/accounts/models/profiles.py
@@ -1,19 +1,16 @@
+from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.db import models
 from .users import User
 
 
 class Profile(models.Model):
-    """
-    Profile class for each user which is being created to hold the information
-    """
-
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=250)
     last_name = models.CharField(max_length=250)
-    image = models.ImageField(blank=True, null=True)
-    description = models.TextField()
+    image = models.ImageField(upload_to="profile_images/", null=True, blank=True)
+    bio = models.TextField(blank=True)
+
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
 
@@ -23,8 +20,5 @@ class Profile(models.Model):
 
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, created, **kwargs):
-    """
-    Signal for post creating a user which activates when a user being created ONLY
-    """
     if created:
         Profile.objects.create(user=instance)
